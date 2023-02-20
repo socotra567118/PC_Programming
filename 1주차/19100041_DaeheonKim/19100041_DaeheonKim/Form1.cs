bb1edd2abae1a9874d17b617018b8c9bdd384933
socotra567118 <89115950+socotra567118@users.noreply.github.com>
2023-02-20 16:20:01 +0900

    2023/02/20 UTC +09:00 16:20

---
+++ 1주차/19100041_DaeheonKim/19100041_DaeheonKim/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _19100041_DaeheonKim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            count.Text = "0";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int currentNumber = Convert.ToInt32(count.Text);
+
+            count.Text = Convert.ToString(currentNumber += 1);
+        }
+    }
+}
