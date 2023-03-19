e2fe19577eba6a3ecd0c2bc7b807f5dfa5ef0b34
socotra567118 <89115950+socotra567118@users.noreply.github.com>
2023-03-19 14:10:34 +0900

    4주차 업로드

---
+++ 4주차/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace _19100041_DaeheonKim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DrawButton = new System.Windows.Forms.Button();
+            this.GraphicBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.GraphicBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DrawButton
+            // 
+            this.DrawButton.Location = new System.Drawing.Point(949, 12);
+            this.DrawButton.Name = "DrawButton";
+            this.DrawButton.Size = new System.Drawing.Size(223, 99);
+            this.DrawButton.TabIndex = 0;
+            this.DrawButton.Text = "Draw";
+            this.DrawButton.UseVisualStyleBackColor = true;
+            this.DrawButton.Click += new System.EventHandler(this.DrawButton_Click);
+            // 
+            // GraphicBox
+            // 
+            this.GraphicBox.Location = new System.Drawing.Point(12, 12);
+            this.GraphicBox.Name = "GraphicBox";
+            this.GraphicBox.Size = new System.Drawing.Size(931, 758);
+            this.GraphicBox.TabIndex = 1;
+            this.GraphicBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 782);
+            this.Controls.Add(this.GraphicBox);
+            this.Controls.Add(this.DrawButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.GraphicBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DrawButton;
+        private System.Windows.Forms.PictureBox GraphicBox;
+    }
+}
+
---
+++ 4주차/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _19100041_DaeheonKim
+{
+    public partial class Form1 : Form
+    {
+
+        double xMin = -2;
+        double xMax = 7;
+        double yMin = -5;
+        double yMax = 70;
+
+        double[] xPoint = new double[6] { 1, 2, 3, 4, 5, 6 };
+        double[] yPoint = new double[6] { 2.1, 7.7, 13.6, 27.2, 40.9, 61.1 };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawButton_Click(object sender, EventArgs e)
+        {
+            Graphics graphic = GraphicBox.CreateGraphics();
+            DrawCoordinate(graphic);
+            DrawPoints(graphic);
+            DrawGraph(graphic);
+        }
+
+        private float ReturnXPoint(double xPoint)
+        {
+            return (float)(GraphicBox.ClientSize.Width * (xPoint - xMin) / (xMax - xMin));
+        }
+
+        private float ReturnYPoint(double yPoint)
+        {
+            return (float)(GraphicBox.ClientSize.Height * (yPoint - yMin) / (yMax - yMin));
+        }
+
+        private void DrawCoordinate(Graphics graphic)
+        {
+            graphic.DrawLine(new Pen(Color.LightGray), ReturnXPoint(xMin), ReturnYPoint(0), ReturnXPoint(xMax), ReturnYPoint(0));
+            graphic.DrawLine(new Pen(Color.LightGray), ReturnXPoint(0), ReturnYPoint(yMin), ReturnXPoint(0), ReturnYPoint(yMax));
+        }
+
+        private void DrawPoints(Graphics graphic)
+        {
+            for (int i = 0; i < 6; i+=1)
+            {
+                float xDot = ReturnXPoint(xPoint[i]);
+                float yDot = ReturnYPoint(yPoint[i]);
+                graphic.DrawEllipse(new Pen(Color.Black), xDot, yDot, 1, 1);
+            }
+        }
+
+        private void DrawGraph(Graphics graphics)
+        {
+            double sumX = 0, sumY = 0, sumXY = 0, sumXX = 0;
+
+            for (int i = 0; i < 6;i+=1)
+            {
+                sumX += xPoint[i];
+                sumY += yPoint[i];
+                sumXY += xPoint[i] * yPoint[i];
+                sumXX += xPoint[i] * xPoint[i];
+            }
+
+            double a = (6 * sumXY - sumX * sumY) / (6 * sumXX - sumX * sumX);
+            double b = (sumY - a * sumX) / 6;
+            double y1 = a * xMin + b;
+            double y2 = a * xMax + b;
+
+            graphics.DrawLine(new Pen(Color.Red), ReturnXPoint(xMin), ReturnYPoint(y1), ReturnXPoint(xMax), ReturnYPoint(y2));
+        }
+    }
+}
