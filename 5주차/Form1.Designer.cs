2eb4eeaafc4eee0677994e679bed0f8569ec1dbc
socotra567118 <89115950+socotra567118@users.noreply.github.com>
2023-03-20 15:12:23 +0900

    5주차

---
+++ 5주차/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace _19100041_DaeheonKim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DotArea = new System.Windows.Forms.PictureBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DotArea)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DotArea
+            // 
+            this.DotArea.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.DotArea.Location = new System.Drawing.Point(12, 12);
+            this.DotArea.Name = "DotArea";
+            this.DotArea.Size = new System.Drawing.Size(1086, 917);
+            this.DotArea.TabIndex = 0;
+            this.DotArea.TabStop = false;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(1104, 12);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(169, 86);
+            this.StartButton.TabIndex = 1;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1285, 941);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.DotArea);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DotArea)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox DotArea;
+        private System.Windows.Forms.Button StartButton;
+    }
+}
+
---
+++ 5주차/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _19100041_DaeheonKim
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+
+        static int dotNumber = 100000;
+
+        int[] xPositions = new int[dotNumber];
+        int[] yPositions = new int[dotNumber];
+
+        (int x, int y) firstPosition = (160, 70);
+        (int x, int y) secondPosition = (50, 410);
+        (int x, int y) thirdPosition = (340, 290);
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.MinimumSize = new Size(400, 500);
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            Make1000Dots();
+            PaintDots();
+        }
+
+        private void Make1000Dots()
+        {
+            for (int i = 0; i < dotNumber; i += 1)
+            {
+                xPositions[i] = random.Next(0, DotArea.ClientSize.Width + 1);
+                yPositions[i] = random.Next(0, DotArea.ClientSize.Height + 1);
+            }
+        }
+
+        private void PaintDots()
+        {
+            Graphics graphics = DotArea.CreateGraphics();
+            Pen lightGrayPen = new Pen(Color.LightGray);
+            Pen blackPen = new Pen(Color.Black);
+
+            for (int i = 0; i < dotNumber; i += 1)
+            {
+                int xDot = xPositions[i];
+                int yDot = yPositions[i];
+
+                double firstArea = ReturnArea(first: (xDot, yDot), second: firstPosition, third: secondPosition);
+                double secondArea = ReturnArea(first: (xDot, yDot), second: thirdPosition, third: firstPosition);
+                double thirdArea = ReturnArea(first: (xDot, yDot), second: secondPosition, third: thirdPosition);
+
+                if (IsInTriangle(first: firstArea, second: secondArea, third: thirdArea))
+                {
+                    graphics.DrawEllipse(blackPen, xDot, yDot, 1, 1);
+                } else
+                {
+                    graphics.DrawEllipse(lightGrayPen, xDot, yDot, 1, 1);
+                }
+
+                
+            }
+        }
+
+        private bool IsInTriangle(double first, double second, double third)
+        {
+            if (first < 0 && second < 0 && third < 0)
+            {
+                return true;
+            } else if (first > 0 && second > 0 && third > 0)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        private double ReturnArea((int xPosition, int yPosition) first, (int xPosition, int yPosition) second, (int xPosition, int yPosition) third)
+        {
+            int firstElement = first.xPosition * second.yPosition +
+                               second.xPosition * third.yPosition +
+                               third.xPosition * first.yPosition;
+
+            int secondElement = first.xPosition * third.yPosition +
+                               second.xPosition * first.yPosition +
+                               third.xPosition * second.yPosition;
+
+            return 0.5 * Convert.ToDouble(firstElement - secondElement);
+        }
+    }
+}
